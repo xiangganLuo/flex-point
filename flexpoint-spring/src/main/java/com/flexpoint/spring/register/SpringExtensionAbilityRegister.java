176f61cbf656dff18af85c9f6d11cc7174ddcbc8
luoxianggan <xiangganluo@gmail.com>
2025-07-04 11:38:47 +0800

    build(flexpoint): 调整项目依赖结构并支持 Spring Boot 自动配置
    - 移除 flexpoint-common 中的 jakarta.validation、jakarta.servlet 和 jakarta.annotation 依赖
    - 在 flexpoint-examples/spring-boot-example 中添加上述依赖
    - 更新 flexpoint-springboot 中 Spring Boot 相关依赖为 provided 范围
    - 新增 META-INF/spring.factories 文件实现 Spring Boot 自动配置- 重构 SpringExtensionAbilityRegister 类，实现 InitializingBean 接口

--- flexpoint-spring/src/main/java/com/flexpoint/spring/register/SpringExtensionAbilityRegister.java
+++ flexpoint-spring/src/main/java/com/flexpoint/spring/register/SpringExtensionAbilityRegister.java
@@ -2,10 +2,10 @@ package com.flexpoint.spring.register;
 
 import com.flexpoint.core.extension.ExtensionAbility;
 import com.flexpoint.core.extension.ExtensionAbilityRegistry;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
@@ -20,36 +20,32 @@ import java.util.Map;
  */
 @Slf4j
 @RequiredArgsConstructor
-public class SpringExtensionAbilityRegister implements ApplicationContextAware {
+public class SpringExtensionAbilityRegister implements ApplicationContextAware, InitializingBean {
 
     private final ExtensionAbilityRegistry extensionAbilityRegistry;
     private ApplicationContext applicationContext;
-
-    @SuppressWarnings("unchecked")
-    @PostConstruct
-    public void initAbilities() {
-        Map<String, ExtensionAbility> beans = applicationContext.getBeansOfType(ExtensionAbility.class);
-        
-        int registeredCount = 0;
-        for (Map.Entry<String, ExtensionAbility> entry : beans.entrySet()) {
-            ExtensionAbility ability = entry.getValue();
-            String beanName = entry.getKey();
-            
-            Class<? extends ExtensionAbility> abilityClass = ability.getClass();
-            Class<?>[] interfaces = abilityClass.getInterfaces();
-            
-            for (Class<?> anInterface : interfaces) {
-                if (ExtensionAbility.class.isAssignableFrom(anInterface)) {
-                    extensionAbilityRegistry.register(ability);
-                    registeredCount++;
-                }
-            }
-        }
-    }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         this.applicationContext = applicationContext;
     }
 
-} 
+    @Override
+    public void afterPropertiesSet() {
+        Map<String, ExtensionAbility> beans = applicationContext.getBeansOfType(ExtensionAbility.class);
+
+        for (Map.Entry<String, ExtensionAbility> entry : beans.entrySet()) {
+            ExtensionAbility ability = entry.getValue();
+            String beanName = entry.getKey();
+
+            Class<? extends ExtensionAbility> abilityClass = ability.getClass();
+            Class<?>[] interfaces = abilityClass.getInterfaces();
+
+            for (Class<?> anInterface : interfaces) {
+                if (ExtensionAbility.class.isAssignableFrom(anInterface)) {
+                    extensionAbilityRegistry.register(ability);
+                }
+            }
+        }
+    }
+}
