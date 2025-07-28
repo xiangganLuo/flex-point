e6cd32291a3b752f9492fa751fb0dca31164d2e4
xiangganluo <xiangganluo@gmail.com>
2025-07-28 05:26:21 +0000

    style(monitor)： 格式问题

--- flexpoint-core/src/main/java/com/flexpoint/core/monitor/enums/MonitorType.java
+++ flexpoint-core/src/main/java/com/flexpoint/core/monitor/enums/MonitorType.java
@@ -4,14 +4,14 @@ package com.flexpoint.core.monitor.enums;
      * 监控器类型
  * @author xiangganluo
  */
-    public enum MonitorType {
-        /**
-         * 默认监控器
-         */
-        DEFAULT,
-        
-        /**
-         * 异步监控器
-         */
-        ASYNC,
-    }
+public enum MonitorType {
+    /**
+     * 默认监控器
+     */
+    DEFAULT,
+    
+    /**
+     * 异步监控器
+     */
+    ASYNC,
+}
